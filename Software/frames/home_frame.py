fc749a268597c470d6e8daf4b3ad7e3cdef5a388
Hernan Rodriguez <hernan.rod.98@gmail.com>
2024-09-17 23:44:47 -0300

    Mejoro interfaz. Aun falta hacerla responsive

--- Software/frames/home_frame.py
+++ Software/frames/home_frame.py
@@ -9,14 +9,64 @@ class InstantValuesFrame(ctk.CTkFrame):
     def __init__(self, master):
         super().__init__(master) 
 
-        self.grid_columnconfigure((0, 1, 2), weight=0)
+        image_path = os.path.join(os.getcwd(), "images")
 
-        left_frame = ctk.CTkFrame(self, fg_color="blue")
-        left_frame.grid(row=0, column=0, padx=0, pady=(0, 20), sticky="w")
-        center_frame = ctk.CTkFrame(self, fg_color="green")
-        center_frame.grid(row=0, column=1, padx=0, pady=(0, 20), sticky="w")
-        right_frame = ctk.CTkFrame(self, fg_color="pink")
-        right_frame.grid(row=0, column=2, padx=0, pady=(0, 20), sticky="w")
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_columnconfigure(2, weight=1)
+
+        self.left_frame = ctk.CTkFrame(self)
+        self.left_frame.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
+
+        self.label_co2 = ctk.CTkLabel(self.left_frame, text="CO2", font=ctk.CTkFont(weight="bold"))
+        self.label_co2.grid(row=0, column=0, padx=10, pady=(10,0), sticky="nsew")
+        self.co2_button = ctk.CTkButton(self.left_frame, text="Encendido", fg_color="green", text_color_disabled="white", hover=False, state="disabled")
+        self.co2_button.grid(row=1, column=0, padx=10, pady=0, sticky="nsew")
+
+        self.label_o2 = ctk.CTkLabel(self.left_frame, text="O2", font=ctk.CTkFont(weight="bold"))
+        self.label_o2.grid(row=2, column=0, padx=10, pady=(10,0), sticky="nsew")
+        self.o2_button = ctk.CTkButton(self.left_frame, text="Encendido", fg_color="green", text_color_disabled="white", hover=False, state="disabled")
+        self.o2_button.grid(row=3, column=0, padx=10, pady=0, sticky="nsew")
+
+        self.label_air = ctk.CTkLabel(self.left_frame, text="Aire", font=ctk.CTkFont(weight="bold"))
+        self.label_air.grid(row=4, column=0, padx=10, pady=(10,0), sticky="nsew")
+        self.air_button = ctk.CTkButton(self.left_frame, text="Encendido", fg_color="green", text_color_disabled="white", hover=False, state="disabled")
+        self.air_button.grid(row=5, column=0, padx=10, pady=0, sticky="nsew")
+
+        self.label_pump = ctk.CTkLabel(self.left_frame, text="Bomba", font=ctk.CTkFont(weight="bold"))
+        self.label_pump.grid(row=6  , column=0, padx=10, pady=(10,0), sticky="nsew")
+        self.pump_button = ctk.CTkButton(self.left_frame, text="Encendido", fg_color="green", text_color_disabled="white", hover=False, state="disabled")
+        self.pump_button.grid(row=7, column=0, padx=10, pady=0, sticky="nsew")
+
+        self.center_frame = ctk.CTkFrame(self)
+        self.center_frame.grid(row=0, column=1, padx=10, pady=10, sticky="nsew")
+
+        self.reactor_img = ctk.CTkImage(Image.open(os.path.join(image_path, "reactor.png")), size=(150, 300))
+        self.reactor_img_label = ctk.CTkLabel(self.center_frame, text="", image=self.reactor_img)
+        self.reactor_img_label.grid(row=0, column=0, padx=0, pady=0, sticky="nsew")
+
+        self.right_frame = ctk.CTkFrame(self)
+        self.right_frame.grid(row=0, column=2, padx=10, pady=10, sticky="nsew")
+
+        self.label_light = ctk.CTkLabel(self.right_frame, text="Luz", font=ctk.CTkFont(weight="bold"))
+        self.label_light.grid(row=0, column=0, padx=10, pady=(10,0), sticky="nsew")
+        self.light_button = ctk.CTkButton(self.right_frame, text="42%", fg_color="white", hover=False, state="disabled", text_color_disabled="black")
+        self.light_button.grid(row=1, column=0, padx=10, pady=0, sticky="nsew")
+
+        self.label_ph = ctk.CTkLabel(self.right_frame, text="pH", font=ctk.CTkFont(weight="bold"))
+        self.label_ph.grid(row=2, column=0, padx=10, pady=(10,0), sticky="nsew")
+        self.ph_button = ctk.CTkButton(self.right_frame, text="6.536", fg_color="white", hover=False, state="disabled", text_color_disabled="black")
+        self.ph_button.grid(row=3, column=0, padx=10, pady=0, sticky="nsew")
+
+        self.label_do = ctk.CTkLabel(self.right_frame, text="DO", font=ctk.CTkFont(weight="bold"))
+        self.label_do.grid(row=4, column=0, padx=10, pady=(10,0), sticky="nsew")
+        self.do_button = ctk.CTkButton(self.right_frame, text="342.4", fg_color="white", hover=False, state="disabled", text_color_disabled="black")
+        self.do_button.grid(row=5, column=0, padx=10, pady=0, sticky="nsew")
+
+        self.label_temp = ctk.CTkLabel(self.right_frame, text="Temperatura", font=ctk.CTkFont(weight="bold"))
+        self.label_temp.grid(row=6  , column=0, padx=10, pady=(10,0), sticky="nsew")
+        self.temp_button = ctk.CTkButton(self.right_frame, text="24.3°C", fg_color="white", hover=False, state="disabled", text_color_disabled="black")
+        self.temp_button.grid(row=7, column=0, padx=10, pady=0, sticky="nsew")
 
     def co2_button_event(self):
         if self.co2_button.cget("text") == "Encendido":
