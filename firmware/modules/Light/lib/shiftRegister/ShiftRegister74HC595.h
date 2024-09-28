8f56071185b0b1b4a32f40c29f0f6ef236a0ec0a
Matias Pardo Errecarret <matipardo99@gmail.com>
2024-09-28 17:33:32 -0300

    Agrego la clase ShiftRegister para el modulo de salidas de bombas y electrovalvulas.

---
+++ firmware/modules/Light/lib/shiftRegister/ShiftRegister74HC595.h
@@ -0,0 +1,31 @@
+#ifndef SHIFTREGISTER74HC595_H
+#define SHIFTREGISTER74HC595_H
+
+#include <Arduino.h>
+#include <ArduinoLog.h>
+
+class ShiftRegister74HC595 {
+  private:
+    int dataPin;   // Pin de DATA
+    int latchPin;  // Pin de LATCH
+    int clockPin;  // Pin de CLOCK
+    byte outputState;  // Estado actual de las salidas del shift register
+
+  public:
+    // Constructor
+    ShiftRegister74HC595(int data, int latch, int clock);
+
+    // Actualiza el estado de una salida específica
+    void setOutput(int pin, bool state);
+
+    // Actualiza el registro de desplazamiento con el estado actual
+    void updateShiftRegister();
+
+    // Obtener el estado actual de las salidas
+    byte getOutputState();
+};
+
+
+
+
+#endif
