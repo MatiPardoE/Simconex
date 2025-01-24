4ebefba973718ec29b000d8b6bb04586ded2c640
Matias Pardo Errecarret <matipardo99@gmail.com>
2025-01-24 00:06:22 -0300

    ADD manual mode implementation with activation, deactivation, and alarm handling

---
+++ firmware/app/lib/manual_mode/manualMode.cpp
@@ -0,0 +1,57 @@
+#include "manualMode.h"
+
+volatile bool ManualMode::manualAlarmFlag = false;
+
+ManualMode::ManualMode()
+{
+    // Constructor implementation
+}
+
+void ManualMode::activate()
+{
+    if (active)
+    {
+        return;
+    }
+    ESP_LOGI("ManualMode", "Activating manual mode");
+    active = true;
+    manualAlarm.setAlarm(2, onManualAlarm);
+}
+
+bool ManualMode::isActive()
+{
+    return active;
+}
+
+ManualMode::ManualBundle ManualMode::run()
+{
+    ManualBundle bundle;
+    if (manualAlarmFlag)
+    {
+        ESP_LOGI("ManualMode", "Alarma de modo manual mode");
+        manualAlarmFlag = false;
+        bundle = ManualBundle::SEND_MEASURES;
+    }
+    else
+    {
+        bundle = ManualBundle::NO_COMMAND;
+    }
+    return bundle;
+}
+
+void ManualMode::onManualAlarm(void *arg)
+{
+    manualAlarmFlag = true;
+}
+
+void ManualMode::deactivate()
+{
+    if (!active)
+    {
+        return;
+    }
+    active = false;
+    manualAlarmFlag = false;
+    manualAlarm.stopAndDeleteAlarm();
+    ESP_LOGI("ManualMode", "Desactivando manual mode");
+}
